a5d471a9207b88b1e290e97d12d003c2c3f8edde
Santyluito <santyluito@gmail.com>
2023-04-27 18:12:28 -0500

    Agregado ssi

--- src/utils/getPokemonInfo.ts
+++ src/utils/getPokemonInfo.ts
@@ -1,13 +1,16 @@
 import pokeApi from "@/api/pokeApi";
 import { PokemonFull } from "@/interfaces";
 
-export const getPokemonInfo = async(nameOrId: string) => {
+export const getPokemonInfo = async (nameOrId: string) => {
+  try {
+    const { data } = await pokeApi.get<PokemonFull>(`/pokemon/${nameOrId}`);
 
-  const { data } = await pokeApi.get<PokemonFull>(`/pokemon/${nameOrId}`);
-
-  return {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites
+    return {
+      id: data.id,
+      name: data.name,
+      sprites: data.sprites,
+    };
+  } catch (error) {
+    return null;
   }
-}
+};
